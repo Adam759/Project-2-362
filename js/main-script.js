e97e445eb17f7d597430c216df319682f6d05bf9
Adam759 <adam759@comcast.net>
2020-04-08 04:12:31 -0500

    More CSS / Scaling Push

---
+++ js/main-script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+  //Navigation Logic and Listeners
+  $(".nav_home").click(function() {
+    document.location.href = "index.html";
+  });
+  $(".nav_consti").click(function() {
+    document.location.href = "constitution.html";
+  });
+  $(".nav_aboutus").click(function() {
+    document.location.href = "about-us.html";
+  });
+  $(".nav_signup").click(function() {
+    document.location.href = "signup.html";
+  });
+  
+});
